5fff85285389516e5e18c0809c4e4e73866a5df0
Marius L <mariusleica@gmail.com>
2022-05-26 16:25:34 +0200

    work in progres

--- palindroma/main.js
+++ palindroma/main.js
@@ -1,2 +1,28 @@
 //Chiedere all’utente di inserire una parola
 // Creare una funzione per capire se la parola inserita è palindroma
+
+/*COME FARE:
+- crea una const con un prompt per chiedere la parola all'utente
+- creiamo una function che divide la parola in lettere e la salva in un array magari in ordine inverso
+- function che controlla se le lettere della parola originale corispondono a quelle della parola rovescia.
+- salva il booleano 
+- stampa il risultato
+*/
+
+const userWord = prompt("Per favore inserisci una parola da verificare!");
+
+function wordDivider(word) {
+  let lettersArray = word.split("");
+  return lettersArray.reverse();
+}
+console.log(wordDivider(userWord));
+
+function isPalindroma(wordToVerify) {
+  let palindroma = false;
+  if (wordToVerify === wordDivider(userWord)) {
+    palindroma = true;
+  }
+  return palindroma;
+}
+
+console.log(isPalindroma());
